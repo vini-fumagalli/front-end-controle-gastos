03fb7a90b285499c1e53b2a3dd91b987e59d5940
Fumas <vinifumagalli_@hotmail.com>
2024-02-25 20:21:29 -0300

    feat (Gastos): implementa modal de update com bootstrap

--- src/endpoints.js
+++ src/endpoints.js
@@ -10,6 +10,8 @@ async function getListaGastos() {
         const response = await axios.get(endpoint);
         console.log(response)
         
+        const total = response.data.resposta.total;
+        const gastoMax = response.data.resposta.gastoMax;
         const listaGastos = response.data.resposta.gastos;
         console.log(listaGastos);
         const tabelaBody = document.querySelector('#tabela tbody');
@@ -31,16 +33,22 @@ async function getListaGastos() {
     
                 tr.innerHTML = `
                 <td><b>${gasto.tipo}<b></td>
-                <td><b>${gasto.valor.toLocaleString('pt-BR')}<b></td>
+                <td><b>${gasto.valor.toFixed(2).replace(".", ",")}<b></td>
                 <td><b>${gasto.pago == true ? 'Pago' : 'Pendente'}<b></td>
                 <td><b>${gasto.dataMax = gasto.pago == true ? '--------------' : gasto.dataMax}<b></td>
                 <td>
-                    <button class="btn btn-primary btn-sm mr-2" style="background-color: #28a745; border-color: #28a745;" onclick="atualizarGasto(${gasto.id})"><i class="fas fa-edit"></i></button>
+                    <button class="btn btn-primary btn-sm mr-2" data-toggle="modal" data-target="#exampleModal" style="background-color: #28a745; border-color: #28a745;" onclick="fillFieldsUpdate('${gasto.tipo}', ${gasto.valor}, ${gasto.pago}, '${gasto.dataMax}')"><i class="fas fa-edit"></i></button>
                     <button class="btn btn-danger btn-sm" onclick="deleteGasto('${gasto.tipo}')"><i class="fas fa-trash-alt"></i></button>
                 </td>
               `;
               tabelaBody.appendChild(tr);
             });
+
+            var totalPagar = document.getElementById('total-pagar');
+            totalPagar.innerText = `${total.toFixed(2).replace(".", ",")} R$`;
+
+            var maximoGasto = document.getElementById('max-gastar');
+            maximoGasto.innerText = `${gastoMax.toFixed(2).replace(".", ",")} R$`;
         }
 
         document.getElementById("loading-overlay").style.display = "none";
@@ -48,6 +56,61 @@ async function getListaGastos() {
         console.error('Erro ao obter a lista de gastos:', error);
         document.getElementById("loading-overlay").style.display = "none";
     }
+}
+
+function fillFieldsUpdate(descricao, valor, situacao, dataMax) {
+
+    situacao = situacao == true ? 'Pago' : 'Pendente';
+
+    if(dataMax == '--------------' || dataMax == null) {
+        document.getElementById('textInput').value = descricao;
+        document.getElementById('numberInput').value = valor;
+        document.getElementById('selectInput').value = situacao;
+        document.getElementById('dateInput').value = null;
+    } else {
+        var partes = dataMax.split('/');
+        var dataFormatada = partes[2] + '-' + partes[1] + '-' + partes[0];
+        dataMax = new Date(dataFormatada).toISOString().substring(0, 10);
+
+        document.getElementById('textInput').value = descricao;
+        document.getElementById('numberInput').value = valor;
+        document.getElementById('selectInput').value = situacao;
+        document.getElementById('dateInput').value = dataMax;
+    }
+
+}
+
+
+async function updateGasto() {
+
+    try {
+        document.getElementById('loading-overlay').style.display = 'block';
+
+        var situacao = document.getElementById('selectInput').value;
+        situacao = situacao == 'Pago' ? true : false;
+
+        var data = document.getElementById('dateInput').value;
+        var dataFormat = new Date(data);
+        
+        var updateGastoDto = {
+            tipo: document.getElementById('textInput').value,
+            valor: document.getElementById('numberInput').value,
+            pago: situacao,
+            dataMax: dataFormat
+        };
+
+        const endpoint = `${API_URL}/gasto`;
+        await axios.put(endpoint, updateGastoDto);
+
+    } catch(error) {
+        console.log(error);
+
+    } finally {
+        document.getElementById('loading-overlay').style.display = 'none';
+        redirectTo('lista-gastos.html');
+    }
+
+    
 }
 
 async function signIn() {
@@ -176,7 +239,7 @@ async function updateSalario() {
 async function deleteGasto(tipo) {
 
     
-    const confirmacao = confirm(`DESPESA SELECIONADA -> ${tipo} \nTem certeza que deseja excluir essa despesa?`);
+    const confirmacao = confirm(`DESPESA SELECIONADA -> ${tipo} \n\nTem certeza que deseja excluir essa despesa?`);
 
     if(confirmacao) {
         try {
@@ -199,4 +262,8 @@ async function deleteGasto(tipo) {
 
 function redirectTo(page) {
     window.location.href = page;
+}
+
+function goBack() {
+    window.history.back();
 }
